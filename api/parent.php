1c0f130e415f82ba1ba813e7a850581a0ab24c69
mrajendray <mrajendray@yahoo.com>
2018-04-29 21:51:13 +0700

    penambahan fungsi melihat daftar permintaan pertemuan dan pertemuan disetujui

--- api/parent.php
+++ api/parent.php
@@ -13,10 +13,10 @@ unset($_GET['do']);
 
 if ($do == 'meeting_request') {
     // check request parameter
-    if ( !isset($_GET['teacher_u_id']) &&
-         !isset($_GET['subject']) &&
-         !isset($_GET['message']) &&
-         !isset($_GET['date'])
+    if (!isset($_GET['teacher_u_id']) &&
+        !isset($_GET['subject']) &&
+        !isset($_GET['message']) &&
+        !isset($_GET['date'])
     ) {
         die('{"status":200, "error":"parameter"}');
     }
@@ -24,8 +24,8 @@ if ($do == 'meeting_request') {
     // write request ke db
     // persiapan sql
     $sql = "INSERT INTO `meeting_request` (`parent_u_id`,`teacher_u_id`,`subject`,`message`,`date`,`status`)";
-    $sql = $sql."VALUES (?, ?, ?, ?, ?, ?)";
-    if($stmt = mysqli_prepare($dbLink, $sql)) {
+    $sql = $sql . "VALUES (?, ?, ?, ?, ?, ?)";
+    if ($stmt = mysqli_prepare($dbLink, $sql)) {
         // bind parameter ke sql
         mysqli_stmt_bind_param($stmt, "iissss", $param_parent_u_id,
             $param_teacher_u_id, $param_subject, $param_message, $param_date, $param_status);
@@ -33,11 +33,11 @@ if ($do == 'meeting_request') {
         $param_parent_u_id = $_SESSION['u_id'];
         $param_teacher_u_id = $_GET['teacher_u_id'];
         $param_subject = $_GET['subject'];
-        $param_message  = $_GET['message'];
+        $param_message = $_GET['message'];
         $param_date = date("Y-m-d", strtotime($_GET['date']));
         $param_status = 'waiting';
         // memulai ekseskusi sql
-        if(!mysqli_stmt_execute($stmt)) {
+        if (!mysqli_stmt_execute($stmt)) {
             die('{"status":200, "meeting_request":"database_error"}');
         }
         // tutup sql
@@ -52,6 +52,56 @@ if ($do == 'meeting_request') {
     // TODO: kirim notifikasi ke guru
 
     echo '{"status":200, "meeting_request":"succeeded"}';
+} elseif ($do=="check_request_status") {
+    // prepare sql
+    $requestList = "[";
+    $sql = "SELECT * FROM `meeting_request` WHERE `parent_u_id` = " . $_SESSION['u_id'];
+    if ($result = mysqli_query($dbLink, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                if ($requestList != "[") $requestList .= ", ";
+
+                $request = "{";
+                $request .= '"request_id":"' . $row["request_id"] . '", ';
+                $request .= '"parent_u_id":"' . $row["parent_u_id"] . '", ';
+                $request .= '"teacher_u_id":"' . $row["teacher_u_id"] . '", ';
+                $request .= '"subject":"' . $row["subject"] . '", ';
+                $request .= '"message":"' . $row["message"] . '", ';
+                $request .= '"date":"' . $row["date"] . '", ';
+                $request .= '"status":"' . $row["status"] . '"';
+                $request .= "}";
+
+                $requestList .= $request;
+            }
+        }
+    }
+    $requestList .= ']';
+    echo '{"status":200, "do":"succeeded", "requestList":' . $requestList . '}';
+} elseif ($do=="check_meeting") {
+    // prepare sql
+    $meetingList = "[";
+    $sql = "SELECT * FROM `meeting` WHERE `meeting_request_id` IN ";
+    $sql .= "(SELECT `request_id` FROM `meeting_request` WHERE `parent_u_id` = ".$_SESSION['u_id'].")";
+    if ($result = mysqli_query($dbLink, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                if ($meetingList != "[") $requestList .= ", ";
+
+                $meeting = "{";
+                $meeting .= '"meeting_id":"' . $row["meeting_id"] . '", ';
+                $meeting .= '"meeting_request_id":"' . $row["meeting_request_id"] . '", ';
+                $meeting .= '"location":"' . $row["location"] . '", ';
+                $meeting .= '"time":"' . $row["time"] . '", ';
+                $meeting .= '"note":"' . $row["note"] . '", ';
+                $meeting .= '"status":"' . $row["status"] . '"';
+                $meeting .= "}";
+
+                $meetingList .= $meeting;
+            }
+        }
+    }
+    $meetingList .= ']';
+    echo '{"status":200, "do":"succeeded", "meetingList":' . $meetingList . '}';
 } else {
     die('{"status":200, "error":"unknown_api"}');
 }
