95327594d9448c77a8db8a400109003540640a07
mrajendray <mrajendray@yahoo.com>
2018-04-29 21:17:57 +0700

    penambahan fungsi melihat daftar permintaan pertemuan

--- api/teacher.php
+++ api/teacher.php
@@ -3,28 +3,42 @@
 require_once 'Warehouse.php';
 
 if ( !isset($_SESSION['profile']) || $_SESSION['profile'] != 'teacher' ) {
-    echo '{"status":200, "error":"no_privilege"}';
-    exit;
+    die('{"status":200, "error":"no_privilege"}');
 } elseif ( !isset($_GET['do']) ) {
-    echo '{"status":200, "error":"unknown_api"}';
-    exit;
+    die('{"status":200, "error":"unknown_api"}');
 }
 
 $do = $_GET['do'];
 unset($_GET['do']);
 
-if ($do == 'meeting_request_accept') {
+if ($do == 'check_meeting_request') {
+    // prepare sql
+    $requestList = "[";
+    $sql = "SELECT * FROM `meeting_request` WHERE `teacher_u_id` = ".$_SESSION['u_id']." AND `status` = 'waiting' ";
+    if($result = mysqli_query($dbLink, $sql)) {
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                if ($requestList != "[") $requestList .= ", ";
 
-    if ( !isset($_GET['meeting_request_id']) ) {
-        echo '{"status":200, "error":"parameter"}';
-        exit;
+                $request = "{";
+                $request .= '"request_id":"'.$row["request_id"].'", ';
+                $request .= '"parent_u_id":"'.$row["parent_u_id"].'", ';
+                $request .= '"teacher_u_id":"'.$row["teacher_u_id"].'", ';
+                $request .= '"subject":"'.$row["subject"].'", ';
+                $request .= '"message":"'.$row["message"].'", ';
+                $request .= '"date":"'.$row["date"].'", ';
+                $request .= '"status":"'.$row["status"].'"';
+                $request .= "}";
+
+                $requestList .= $request;
+            }
+        }
     }
-
-    // write accept ke db
-    // kirim notifikasi ke orangtua
-
-    echo '{"status":200, "do":"succeeded"}';
+    $requestList .= ']';
+    echo '{"status":200, "do":"succeeded", "requestList":'.$requestList.'}';
 } else {
-    echo '{"status":200, "error":"unknown_api"}';
-    exit;
+    die('{"status":200, "error":"unknown_api"}');
 }
+
+// write accept ke db
+// TODO: kirim notifikasi ke orangtua
